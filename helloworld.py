bb40c7e9b731fd5d99bc37a43d353e5b551bee5e
Sarah Azouvi <sarah_azouvi@hotmail.com>
2016-03-29 18:44:01 +0100

    working version

--- helloworld.py
+++ helloworld.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
  
 import tweepy, time, sys
+reload(sys)  
+sys.setdefaultencoding('utf8')
+ 
 #argfile = str(sys.argv[1])
  
 #enter the corresponding information from your Twitter application:
 from keys import keys
- 
 CONSUMER_KEY = keys['consumer_key']
 CONSUMER_SECRET = keys['consumer_secret']
 ACCESS_KEY = keys['access_token']
@@ -42,14 +44,12 @@ replies=all
 #             time.sleep(900)
 #             
 #twt = api.search(q="Hello World!")     
-# list =['Happiness is not the absence of problems, it’s the ability to deal with them.-Steve Maraboli',
-# 'You are here in this world to make a positive difference.',
-# 'You have the power to spend your life with people who truly matter to you.']
+ 
 
 class CustomStreamListener(tweepy.StreamListener):
     def on_status(self, s):
     	sn=s.user.screen_name
-    	m="@%s Happiness is not the absence of problems, it’s the ability to deal with them.-Steve Maraboli" % (sn)
+    	m="@%s Be believing, be happy, don’t get discouraged. Things will work out. Gordon B. Hinckley" % (sn)
     	api.update_status(m,in_reply_to_status_id = s.id)
     	
     def on_error(self, status_code):
@@ -61,4 +61,4 @@ class CustomStreamListener(tweepy.StreamListener):
         return True # Don't kill the stream
 
 sapi = tweepy.streaming.Stream(auth, CustomStreamListener())
-sapi.filter(track=['feel sad'])
+sapi.filter(track=['feel so sad'])
