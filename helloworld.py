e358ad106052779aaf5f0e3c0e78aedc110b99e9
Sarah Azouvi <sarah_azouvi@hotmail.com>
2016-03-29 21:51:58 +0100

    latest version that reply to users

--- helloworld.py
+++ helloworld.py
@@ -21,7 +21,14 @@ replies=all
 list =['Happiness is not the absence of problems, it’s the ability to deal with them.-Steve Maraboli',
 'You are here in this world to make a positive difference.',
 'You have the power to spend your life with people who truly matter to you.',
-'Be believing, be happy, don’t get discouraged. Things will work out. Gordon B. Hinckley']  
+'Be believing, be happy, don’t get discouraged. Things will work out. Gordon B. Hinckley',
+'It doesn’t matter how many times you fail; what matters is how many times you stand up and try again.',
+'The best is yet to come.',
+'Indulging in chocolate every once in a while is always a good idea.',
+'Time heals almost everything, so give time some time to work.',
+'You can get rid of anything that isn’t useful, beautiful or joyful in your life.',
+'Fear less, and hope more; Whine less, and breathe more; Talk less, and listen more; and hate less, so you can love more',
+'Look for the rainbow in every storm!']  
  
 
 class CustomStreamListener(tweepy.StreamListener):
@@ -39,4 +46,4 @@ class CustomStreamListener(tweepy.StreamListener):
         return True # Don't kill the stream
 
 sapi = tweepy.streaming.Stream(auth, CustomStreamListener())
-sapi.filter(track=['feel so sad'])
+sapi.filter(track=['@un_peu_d_amour'])
